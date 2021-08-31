c65c2482a54f957799b5a1c5cbb58d6e6a9998a5
Mark Stefanovic <markstefanovic@hotmail.com>
2021-08-30 18:25:00 -0700

    add test for sync criteria

--- src/test/kotlin/kda/SyncTest.kt
+++ src/test/kotlin/kda/SyncTest.kt
@@ -1,6 +1,12 @@
 package kda
 
+import kda.domain.Criteria
 import kda.domain.Dialect
+import kda.domain.Field
+import kda.domain.Operator
+import kda.domain.Predicate
+import kda.domain.StringType
+import kda.domain.StringValue
 import kda.domain.SyncResult
 import kda.shared.connect
 import kda.shared.tableExists
@@ -19,16 +25,28 @@ fun fetchCustomers(con: Connection, tableName: String): Set<Customer> {
   con.createStatement().use { stmt ->
     stmt.executeQuery(sql).use { rs ->
       while (rs.next()) {
-        val customer = Customer(
-          customerId = rs.getInt(0),
-          firstName = rs.getString(1),
-          lastName = rs.getString(2),
-        )
+        val customer =
+          Customer(
+            customerId = rs.getInt(1),
+            firstName = rs.getString(2),
+            lastName = rs.getString(3),
+          )
         customers.add(customer)
       }
     }
   }
   return customers.toSet()
+}
+
+fun addCustomers(con: Connection, customers: List<Customer>) {
+  for (customer in customers) {
+    val sql =
+      """
+      INSERT INTO sales.customer (customer_id, first_name, last_name)
+      VALUES (${customer.customerId}, '${customer.firstName}', '${customer.lastName}')
+    """
+    con.createStatement().use { stmt -> stmt.execute(sql) }
+  }
 }
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -49,17 +67,6 @@ class SyncTest {
             )
             """.trimIndent()
           )
-          stmt.execute(
-            """
-            INSERT INTO sales.customer (customer_id, first_name, last_name)
-            VALUES 
-                (1, 'Mark', 'Stefanovic')
-            ,   (2, 'Bob', 'Smith')
-            ,   (3, 'Mandie', 'Mandlebrot')
-            ,   (4, 'Mark', 'Smith')
-            ,   (5, 'Jenny', 'Smith')
-            """
-          )
         }
         assertFalse(tableExists(destCon, schema = "sales", table = "customer2"))
       }
@@ -70,6 +77,14 @@ class SyncTest {
   fun when_dest_table_is_empty_then_all_rows_added() {
     connect().use { srcCon ->
       connect().use { destCon ->
+        val customers =
+          listOf(
+            Customer(customerId = 1, firstName = "Mark", lastName = "Stefanovic"),
+            Customer(customerId = 2, firstName = "Bob", lastName = "Smith"),
+            Customer(customerId = 3, firstName = "Mandie", lastName = "Mandlebrot"),
+          )
+        addCustomers(con = srcCon, customers = customers)
+
         val result =
           sync(
             srcCon = srcCon,
@@ -91,38 +106,70 @@ class SyncTest {
             srcTable = "customer",
             destSchema = "sales",
             destTable = "customer2",
-            added = 5,
+            added = 3,
+            deleted = 0,
+            updated = 0,
+          )
+        assertEquals(expected = expectedSyncResult, actual = result)
+
+        val actualRows = fetchCustomers(con = destCon, tableName = "customer2")
+
+        assertEquals(expected = customers.toSet(), actual = actualRows)
+      }
+    }
+  }
+
+  @Test
+  fun sync_with_criteria_should_only_refresh_rows_that_match_criteria() {
+    connect().use { srcCon ->
+      connect().use { destCon ->
+        val customers =
+          listOf(
+            Customer(customerId = 1, firstName = "Mark", lastName = "Stefanovic"),
+            Customer(customerId = 2, firstName = "Bob", lastName = "Smith"),
+            Customer(customerId = 3, firstName = "Mandie", lastName = "Smith"),
+          )
+        addCustomers(con = srcCon, customers = customers)
+
+        val result =
+          sync(
+            srcCon = srcCon,
+            destCon = destCon,
+            srcDialect = Dialect.PostgreSQL,
+            destDialect = Dialect.PostgreSQL,
+            srcSchema = "sales",
+            srcTable = "customer",
+            destSchema = "sales",
+            destTable = "customer2",
+            compareFields = setOf("first_name"),
+            primaryKeyFieldNames = listOf("customer_id"),
+            includeFields = null,
+            criteria = listOf(
+              Criteria(
+                listOf(Predicate(
+                  field = Field(name = "last_name", dataType = StringType(40)),
+                  operator = Operator.Equals,
+                  value = StringValue(value = "Smith", maxLength = 40),
+                ))
+              )
+            ),
+          )
+
+        val expectedSyncResult =
+          SyncResult.Success(
+            srcSchema = "sales",
+            srcTable = "customer",
+            destSchema = "sales",
+            destTable = "customer2",
+            added = 2,
             deleted = 0,
             updated = 0,
           )
         assertEquals(expected = expectedSyncResult, actual = result)
 
         val expectedRows = setOf(
-          Customer(
-            customerId = 1,
-            firstName = "Mark",
-            lastName = "Stefanovic",
-          ),
-          Customer(
-            customerId = 2,
-            firstName = "Bob",
-            lastName = "Smith",
-          ),
-          Customer(
-            customerId = 3,
-            firstName = "Mandie",
-            lastName = "Mandlebrot",
-          ),
-          Customer(
-            customerId = 4,
-            firstName = "Mark",
-            lastName = "Smith",
-          ),
-          Customer(
-            customerId = 5,
-            firstName = "Jenny",
-            lastName = "Smith",
-          ),
+          Customer(customerId = 2, firstName = "Bob", lastName = "Smith"),
+          Customer(customerId = 3, firstName = "Mandie", lastName = "Smith"),
         )
 
         val actualRows = fetchCustomers(con = destCon, tableName = "customer2")
