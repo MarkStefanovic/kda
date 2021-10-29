e9364a4fd421a94457d73789b3d0cbd94b3d3ff2
Mark Stefanovic <markstefanovic@hotmail.com>
2021-10-29 02:46:05 -0700

    dont unwrap field name when getting value from row

--- src/main/kotlin/kda/domain/Row.kt
+++ src/main/kotlin/kda/domain/Row.kt
@@ -1,7 +1,5 @@
 package kda.domain
 
-val nonAlphaNum = "[^a-zA-Z0-9_]".toRegex()
-
 @JvmInline
 value class Row(private val row: Map<String, Value<*>>) {
   init {
@@ -12,29 +10,21 @@ value class Row(private val row: Map<String, Value<*>>) {
   val fieldNames: Set<String>
     get() = row.keys
 
-  fun value(fieldName: String): Value<*> {
-    val stdFieldName = unwrapName(fieldName)
-    return row[stdFieldName]
-      ?: error(
-        "A field named $stdFieldName was not found in the row.  " +
-          "Available fields include the following: ${row.keys.joinToString(", ")}"
-      )
-  }
+  fun value(fieldName: String): Value<*> =
+    row[fieldName] ?: throw KDAError.FieldNotFound(fieldName = fieldName, availableFieldNames = row.keys)
 
-  fun subset(fieldNames: Set<String>): Row {
-    val stdFieldNames = fieldNames.map(::unwrapName)
-    return if (stdFieldNames.toSet() == row.keys) {
+  fun subset(fieldNames: Set<String>): Row =
+    if (fieldNames.toSet() == row.keys) {
       this
     } else {
-      require(stdFieldNames.all { fldName -> row.containsKey(fldName) })
-      val subset = stdFieldNames.associateWith { fieldName -> row[fieldName]!! }
+      require(fieldNames.all { fldName -> row.containsKey(fldName) })
+      val subset = fieldNames.associateWith { fieldName ->
+        row[fieldName] ?: throw KDAError.FieldNotFound(fieldName = fieldName, availableFieldNames = row.keys)
+      }
       Row(subset)
     }
-  }
 
   companion object {
     fun of(vararg keyValuePairs: Pair<String, Value<*>>): Row = Row(keyValuePairs.toMap())
   }
 }
-
-private fun unwrapName(name: String) = name.replace(nonAlphaNum, "")
