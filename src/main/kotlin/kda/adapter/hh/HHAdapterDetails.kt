91e2c2e93bc9dece889d42597aa686ed222b96cf
Mark Stefanovic <markstefanovic@hotmail.com>
2021-12-13 08:30:55 -0800

    fix adapter not using correct details

--- src/main/kotlin/kda/adapter/hh/HHAdapterDetails.kt
+++ src/main/kotlin/kda/adapter/hh/HHAdapterDetails.kt
@@ -1,9 +1,28 @@
 package kda.adapter.hh
 
 import kda.adapter.std.StdAdapterDetails
+import kda.domain.DataType
 import kda.domain.DbAdapterDetails
+import kda.domain.Field
+import kda.domain.Parameter
 
 @ExperimentalStdlibApi
-object HHAdapterDetails : DbAdapterDetails by StdAdapterDetails {
+object HHAdapterDetails : DbAdapterDetails {
+  private val std = StdAdapterDetails
+
+  override fun fieldDef(field: Field<*>): String = std.fieldDef(field)
+
+  override fun castParameter(dataType: DataType<*>): String = std.castParameter(dataType)
+
+  override fun <T> whereFieldIsEqualTo(field: Field<T>): Set<Parameter> = std.whereFieldIsEqualTo(field)
+
+  override fun <T> whereFieldIsGreaterThan(field: Field<T>): Set<Parameter> = std.whereFieldIsGreaterThan(field)
+
+  override fun <T> whereFieldIsGreaterThanOrEqualTo(field: Field<T>): Set<Parameter> = std.whereFieldIsGreaterThanOrEqualTo(field)
+
+  override fun <T> whereFieldIsLessThan(field: Field<T>): Set<Parameter> = std.whereFieldIsLessThan(field)
+
+  override fun <T> whereFieldIsLessThanOrEqualTo(field: Field<T>): Set<Parameter> = std.whereFieldIsLessThanOrEqualTo(field)
+
   override fun wrapName(name: String): String = "`$name`"
 }
--- src/main/kotlin/kda/adapter/sqlite/SQLiteAdapterDetails.kt
+++ src/main/kotlin/kda/adapter/sqlite/SQLiteAdapterDetails.kt
@@ -1,7 +1,28 @@
 package kda.adapter.sqlite
 
 import kda.adapter.std.StdAdapterDetails
+import kda.domain.DataType
 import kda.domain.DbAdapterDetails
+import kda.domain.Field
+import kda.domain.Parameter
 
 @ExperimentalStdlibApi
-object SQLiteAdapterDetails : DbAdapterDetails by StdAdapterDetails
+object SQLiteAdapterDetails : DbAdapterDetails {
+  private val std = StdAdapterDetails
+
+  override fun fieldDef(field: Field<*>): String = std.fieldDef(field)
+
+  override fun castParameter(dataType: DataType<*>): String = std.castParameter(dataType)
+
+  override fun <T> whereFieldIsEqualTo(field: Field<T>): Set<Parameter> = std.whereFieldIsEqualTo(field)
+
+  override fun <T> whereFieldIsGreaterThan(field: Field<T>): Set<Parameter> = std.whereFieldIsGreaterThan(field)
+
+  override fun <T> whereFieldIsGreaterThanOrEqualTo(field: Field<T>): Set<Parameter> = std.whereFieldIsGreaterThanOrEqualTo(field)
+
+  override fun <T> whereFieldIsLessThan(field: Field<T>): Set<Parameter> = std.whereFieldIsLessThan(field)
+
+  override fun <T> whereFieldIsLessThanOrEqualTo(field: Field<T>): Set<Parameter> = std.whereFieldIsLessThanOrEqualTo(field)
+
+  override fun wrapName(name: String): String = std.wrapName(name)
+}
